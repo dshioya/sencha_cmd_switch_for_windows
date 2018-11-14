675059c9a62e958c92b05e570ff52b47f1a55e0c
d.shioya <d.shioya@wiz-net.jp>
2018-11-15 02:39:09 +0900

    バージョンのみ表示するように修正。現在のバージョンにマークを表示。

--- scl.bat
+++ scl.bat
@@ -3,10 +3,21 @@ rem Senchaコマンドバージョン一覧表示
 rem SENCHA_HOME_PATH は環境に合わせること
 set SENCHA_HOME_PATH=C:\Users\xxxxx\bin\Sencha\Cmd\
 
+set /p VERSION_PROPERTIES_CONTENT=< %SENCHA_HOME_PATH%version.properties
+
+set CURRENT_VERSION=%VERSION_PROPERTIES_CONTENT:~13%
+
 pushd %SENCHA_HOME_PATH%
 
 for /D %%A in (*.*) do (
-    echo %%A
+    echo %%A | findstr /r "\<[0-9][0-9.]*[0-9]\>" >nul 2>&1
+    if not errorlevel 1 (
+        if %%A==%CURRENT_VERSION% (
+            echo * %%A
+        ) else (
+            echo   %%A
+        )
+    )
 )
 
 popd
