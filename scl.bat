233b5c6862b6870b4a3a0b5edb9f0884f6d9ae4f
d.shioya <d.shioya@wiz-net.jp>
2018-11-15 02:14:42 +0900

    sclコマンドの不具合修正

--- scl.bat
+++ scl.bat
@@ -1,6 +1,12 @@
 @echo off
 rem Senchaコマンドバージョン一覧表示
+rem SENCHA_HOME_PATH は環境に合わせること
+set SENCHA_HOME_PATH=C:\Users\xxxxx\bin\Sencha\Cmd\
+
+pushd %SENCHA_HOME_PATH%
 
 for /D %%A in (*.*) do (
     echo %%A
 )
+
+popd
